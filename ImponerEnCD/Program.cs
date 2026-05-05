5120ab1b8f55bd91fd5d1f69ac4ab98f069c4af2
Santiago <smbouche@gmail.com>
2026-05-05 18:58:08 -0300

    Mensaje de prueba para sincronizacion

--- ImponerEnCD/Program.cs
+++ ImponerEnCD/Program.cs
@@ -17,6 +17,8 @@ namespace ImponerEnCD
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //comentario de prueba
         }
     }
 }
